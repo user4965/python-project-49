4889f957e1363187a1a405ff51e3056f920b871f
Vladimir_user4965 <onischenkovs@gmail.com>
2025-11-14 01:16:59 +0300

    fix ruff.lint issues and update configuration

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,9 +1,10 @@
 from prompt import string
 
+
 def greet():
-    print(f'Welcome to the Brain Games! ')
+    print('Welcome to the Brain Games!')
+
 
 def welcome_user():
-    name = string(f'May I have your name? ')
+    name = string('May I have your name? ')
     print(f'Hello, {name}! ')
-
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
-from brain_games.cli import welcome_user, greet
+from brain_games.cli import greet, welcome_user
+
 
 def main():
     greet()
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
