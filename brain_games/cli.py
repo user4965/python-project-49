3c450fd2e08120378db150b4bf124d8eadeddca0
Vladimir_user4965 <onischenkovs@gmail.com>
2025-11-13 23:30:33 +0300

    add cli.py and move greet there, update enterypoint

---
+++ brain_games/cli.py
@@ -0,0 +1,9 @@
+from prompt import string
+
+def greet():
+    print(f'Welcome to the Brain Games! ')
+
+def welcome_user():
+    name = string(f'May I have your name? ')
+    print(f'Hello, {name}! ')
+
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,8 @@
-def greet():
-    print("Welcome to the Brain Games!")
+from brain_games.cli import welcome_user, greet
 
 def main():
     greet()
+    welcome_user()
 
 if __name__ == '__main__':
     main()
-
